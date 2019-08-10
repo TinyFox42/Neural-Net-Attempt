d98d0269b6c723fea563847e1ea1287752395e6f
Eli Thorpe <elithorpe42@gmail.com>
2019-08-10 17:17:08 -0400

    Create basic node structures
    Next I'll make a test net and try it out. But I have to do other stuff right now

---
+++ node_v1.py
@@ -0,0 +1,35 @@
+import math
+class node(object):
+    def __init__(self):
+        self.sources=[]
+        self.weights={} #later on, this should be random values, from a normal distribution centered around 0, sigma=1
+        self.val=None
+        self.loop_catch=False
+    def decide(self):
+        n=0
+        for s in self.sources:
+            n+=self.weights[s]*s.get_val()
+        self.val=node.activation(n)
+    def get_val(self):
+        if self.val==None:
+            if self.loop_catch:
+                raise SyntaxError, "Node called without reset"
+            self.loop_catch=True
+            self.decide() #This has a very weak infinite loop catcher. Put in work to stop infinite loops without throwing errors
+        return self.val
+    def reset(self):
+        #prepare for next run
+        self.val=None
+        self.loop_catch=False
+    @staticmethod  
+    def activation(x):
+        a=1/(1+math.e**(-x))
+        return a
+        
+class start_node(object):
+    def __init__(self):
+        self.val=None
+    def assign(self, x):
+        self.val=x
+    def get_val(self):
+        return self.val
