bc8b87e858593f4694662723cf88484f8ba18ef1
Eli Thorpe <elithorpe42@gmail.com>
2019-08-27 19:46:19 -0400

    Optional sound effect at the end of step
    Tada!

--- pallindrome_test.py
+++ pallindrome_test.py
@@ -1,6 +1,7 @@
 #looks for 4 bit pallindromes
 import pop_v1
 import random
+import winsound
 reload(pop_v1)
 def make_qs(n):
     qs=[]
@@ -17,13 +18,16 @@ def make_qs(n):
         qs+=[[a,b,c,d]]
     return qs, ans
 
-def step(pop):
+def step(pop, alert=False):
     #Calls some functions on the pop. The net will be modified. No real need to return it, so it doesn't
+    #If you pass "True" as the second argument, it will play whatever sound on your (Windows) computer is set to "Question". See control panel/sounds
     qs,ans=make_qs(1000)
     pop.test(qs,ans)
     print pop.fancy_stats()
     pop.cull()
     pop.repop()
+    if alert:
+        winsound.PlaySound("SystemQuestion", winsound.SND_ALIAS)
     
 def make_pop(n):
     return pop_v1.population(4,2,n,[4,4,4])
